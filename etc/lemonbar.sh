7cabff81b6f17fbc66dd8abf78d87bc8199cdb80
Jason <jason@casjay.net>
2021-01-19 06:57:24 -0500

    🦈🏠🐜❗ Changed the install directory ❗🐜🦈🏠

---
+++ etc/lemonbar.sh
@@ -0,0 +1,94 @@
+#!/usr/bin/env bash
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# @Author      : Jason
+# @Contact     : casjaysdev@casjay.net
+# @File        : lemonbar
+# @Created     : Mon, Dec 31, 2019, 00:00 EST
+# @License     : WTFPL
+# @Copyright   : Copyright (c) CasjaysDev
+# @Description : lemonbar script
+#
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+# Set functions
+
+mainbar() {
+  Clock() {
+    DATE=$(date "+%m.%d.%y")
+    TIME=$(date "+%I:%M")
+
+    echo -e -n "\uf073 ${DATE} \uf017 ${TIME}"
+  }
+
+  ActiveWindow() {
+    echo -n "$(xdotool getwindowfocus getwindowname)"
+  }
+
+  Battery() {
+    BATTACPI=$(acpi --battery)
+    BATPERC=$(echo $BATTACPI | cut -d, -f2 | tr -d '[:space:]')
+
+    if [[ $BATTACPI == *"100%"* ]]; then
+      echo -e -n "\uf00c $BATPERC"
+    elif [[ $BATTACPI == *"Discharging"* ]]; then
+      BATPERC=${BATPERC::-1}
+      if [ $BATPERC -le "10" ]; then
+        echo -e -n "\uf244"
+      elif [ $BATPERC -le "25" ]; then
+        echo -e -n "\uf243"
+      elif [ $BATPERC -le "50" ]; then
+        echo -e -n "\uf242"
+      elif [ $BATPERC -le "75" ]; then
+        echo -e -n "\uf241"
+      elif [ $BATPERC -le "100" ]; then
+        echo -e -n "\uf240"
+      fi
+      echo -e " $BATPERC%"
+    elif [[ $BATTACPI == *"Charging"* && $BATTACPI != *"100%"* ]]; then
+      echo -e "\uf0e7 $BATPERC"
+    elif [[ $BATTACPI == *"Unknown"* ]]; then
+      echo -e "$BATPERC"
+    fi
+  }
+
+  Wifi() {
+    WIFISTR=$(iwconfig wlan0 | grep "Link" | sed 's/ //g' | sed 's/LinkQuality=//g' | sed 's/\/.*//g')
+    if [ ! -z $WIFISTR ]; then
+      WIFISTR=$((${WIFISTR} * 100 / 70))
+      ESSID=$(iwconfig wlan0 | grep ESSID | sed 's/ //g' | sed 's/.*://' | cut -d "\"" -f 2)
+      if [ $WIFISTR -ge 1 ]; then
+        echo -e "\uf1eb ${ESSID} ${WIFISTR}%"
+      fi
+    fi
+  }
+
+  Sound() {
+    NOTMUTED=$(amixer sget Master | grep "\[on\]")
+    if [[ ! -z $NOTMUTED ]]; then
+      VOL=$(awk -F"[][]" '/dB/ { print $2 }' <(amixer sget Master) | sed 's/%//g')
+      if [ $VOL -ge 85 ]; then
+        echo -e "\uf028 ${VOL}%"
+      elif [ $VOL -ge 50 ]; then
+        echo -e "\uf027 ${VOL}%"
+      else
+        echo -e "\uf026 ${VOL}%"
+      fi
+    else
+      echo -e "\uf026 M"
+    fi
+  }
+
+  while true; do
+    echo -e "%{l}" "%{c}$(ActiveWindow)" "%{r}$(Wifi)  $(Battery)  $(Clock)"
+    sleep 0.1s
+  done
+}
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+mainbar | lemonbar -p -F#FFFFFFFF -B#FF222222 -f "Hack Nerd Font-8" -f FontAwesome-8 >/dev/null 2>&1 &
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+#end
